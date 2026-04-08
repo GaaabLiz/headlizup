1884f0932ac6f169f297b946d30294aaf718f9ea
gabliz <lizzosgabriele@gmail.com>
2026-04-08 18:58:47 +0200

    feat: add project metadata including name, version, description, and author information

---
+++ headlizup/project.py
@@ -0,0 +1,7 @@
+# fmt: off
+name = 'headlizup'
+version = '0.0.2'
+description = 'Add your description here'
+requires_python = '>=3.13'
+authors = [('gabliz', 'lizzosgabriele@gmail.com')]
+# fmt: on
