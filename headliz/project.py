5d6bb88aa10d697b3d5e97e4a643261e69cd9d81
gabliz <lizzosgabriele@gmail.com>
2026-04-12 19:45:40 +0200

    bump: v0.1.0

--- headliz/project.py
+++ headliz/project.py
@@ -1,6 +1,6 @@
 # fmt: off
 name = 'headliz'
-version = '0.0.2'
+version = '0.1.0'
 description = 'Add your description here'
 requires_python = '>=3.13'
 authors = [('gabliz', 'lizzosgabriele@gmail.com')]
